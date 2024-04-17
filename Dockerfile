4e05e895153036282244c8a444223af778487985
KronosDP <darrel.danadyaksa19@gmail.com>
2024-04-17 17:57:57 +0700

    build gradle back, docker file edit

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,6 @@ ENV JDBC_DATABASE_USERNAME ${JDBC_DATABASE_USERNAME}
 
 WORKDIR /app
 RUN ls -al
-COPY ./authprofile-0.0.1-SNAPSHOT.jar /app
+COPY ./auth-profile-0.0.1-SNAPSHOT.jar /app
 EXPOSE 8080
-CMD ["java","-jar","authprofile-0.0.1-SNAPSHOT.jar"]
+CMD ["java","-jar","auth-profile-0.0.1-SNAPSHOT.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,73 +1,53 @@
 plugins {
-    java
-    jacoco
-    id("org.springframework.boot") version "3.2.4"
-    id("io.spring.dependency-management") version "1.1.4"
+	java
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
+	jacoco
 }
 
-group = "id.ac.ui.cs.advprog"
+group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+	sourceCompatibility = JavaVersion.VERSION_21
 }
 
 configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("org.postgresql:postgresql")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.register<Test>("unitTest") {
-    description = "Runs unit tests."
-    group = "verification"
-
-    filter {
-        excludeTestsMatching("*FunctionalTest")
-    }
-}
-
-tasks.register<Test>("functionalTest") {
-    description = "Runs functional tests."
-    group = "verification"
-
-    filter {
-        includeTestsMatching("*FunctionalTest")
-    }
-}
-
-tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
 
 tasks.test {
-    filter {
-        excludeTestsMatching("*FunctionalTest")
-    }
-
-    finalizedBy(tasks.jacocoTestReport)
+   useJUnitPlatform()
+   finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
-
 tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports {
-        xml.required.set(true)
-        csv.required.set(true)
-        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
-    }
+   classDirectories.setFrom(files(classDirectories.files.map {
+       fileTree(it) { exclude("**/*Application**") }
+   }))
+   dependsOn(tasks.test) // tests are required to run before generating the report
+   reports {
+       xml.required.set(false)
+       csv.required.set(false)
+       html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+   }
 }
