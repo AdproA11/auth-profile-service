23672cd99e3a09da7d0dc37f52d38d72abc1d7dc
KronosDP <darrel.danadyaksa19@gmail.com>
2024-05-15 19:44:12 +0700

    controller to test deployment

---
+++ src/main/java/com/example/authprofile/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.authprofile.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
