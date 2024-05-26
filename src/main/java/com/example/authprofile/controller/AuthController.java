9c65914a9c5e0f010931d93cf2b2e4a69f0e38dd
KronosDP <darrel.danadyaksa19@gmail.com>
2024-05-26 12:17:27 +0700

    chore: Remove userlist.html and update postlogin.html

--- src/main/java/com/example/authprofile/controller/AuthController.java
+++ src/main/java/com/example/authprofile/controller/AuthController.java
@@ -67,13 +67,31 @@ public class AuthController {
         return new ResponseEntity<>(new AuthResponseDto(token), HttpStatus.OK);
     }
 
+    @PostMapping("login")
+    public String loginUser(@ModelAttribute("logindto") LoginDto logindto, Model model) {
+        if (userService.findByUsername(logindto.getUsername()).isPresent()) {
+            UserEntity user = userService.findByUsername(logindto.getUsername()).get();
+            if (passwordEncoder.matches(logindto.getPassword(), user.getPassword())) {
+                return "redirect:/postlogin";
+            }
+        }
+        return "redirect:/login";
+    }
+
+    @GetMapping("/postlogin")
+    public ModelAndView postlogin(Model model) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("postlogin.html");
+        return modelAndView;
+    }
+
     @PostMapping("/logout")
     public ResponseEntity<String> logout() {
         SecurityContextHolder.clearContext();
         return new ResponseEntity<>("Logout success!", HttpStatus.OK);
     }
 
-    @GetMapping
+    @GetMapping("/register")
     public ModelAndView register(Model model) {
         RegisterDto registerDto = new RegisterDto();
         model.addAttribute("registerdto", registerDto);
